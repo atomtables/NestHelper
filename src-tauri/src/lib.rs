3762d20cc5c76b41dc236ec85455608960ef0b43
Adithiya Venkatakrishnan <adithiya.venkatakrishnan@atomtables.dev>
2025-08-07 21:47:50 -0400

    Remove Storybook + Initialise project params

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,16 +1,20 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
-      Ok(())
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![helloworld])
+        .setup(|app| {
+            if cfg!(debug_assertions) {
+                app.handle().plugin(
+                    tauri_plugin_log::Builder::default()
+                        .level(log::LevelFilter::Info)
+                        .build(),
+                )?;
+            }
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
+
+#[tauri::command]
+fn helloworld() {}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script>
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
-	
+	import { getCurrentWindow } from '@tauri-apps/api/window';
+	import {onMount} from "svelte";
+
+	// when using `"withGlobalTauri": true`, you may use
+	// const { getCurrentWindow } = window.__TAURI__.window;
+
 	let { children } = $props();
 </script>
 
@@ -9,4 +14,9 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<div class="h-9 bg-purple-200 dark:bg-purple-900 flex flex-row justify-between items-center" data-tauri-drag-region>
+	<div class="text-sm opacity-0">NestHelper</div>
+	<div class="text-sm absolute top-2 right-1/2 translate-x-1/2">NestHelper</div>
+	<div class="text-sm mr-2">home|etc</div>
+</div>
 {@render children?.()}
